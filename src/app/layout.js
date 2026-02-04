60dc1ce256528d59c85de758eef6a338d26f03b9
Nguyễn Minh Quân <quannguyenkm16122004@gmail.com>
2026-02-04 12:04:50 +0700

    update header footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,11 +6,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="vi">
       <body className="flex flex-col min-h-screen">
-        <Header />
+        {/* <Header /> */}
         <main className="flex-grow">
           {children}
         </main>
-        <Footer />
+        {/* <Footer /> */}
       </body>
     </html>
   );
